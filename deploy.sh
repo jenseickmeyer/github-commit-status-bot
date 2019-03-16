05c209b81784dffffc3cdac586975622ac2db257
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2019-03-16 21:27:54 +0100

    Script for deploying solution

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+S3_BUCKET=
+INPUT_FILE=template.yaml
+OUTPUT_FILE=template-output.yaml
+REGION=eu-central-1
+STACK_NAME=github-commit-status-bot
+
+cd src && npm install && npm run-script lint && npm test && npm prune --production && cd ..
+
+aws cloudformation package --template-file $INPUT_FILE --s3-bucket $S3_BUCKET --output-template-file $OUTPUT_FILE
+aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM --region $REGION
